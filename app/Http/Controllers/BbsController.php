031015001b521b583e60b7fc398c64ad55a98535
Vlad Ageysha <ander123fakk@gmail.com>
2022-09-23 14:01:37 +0300

    Create base layouts for other views

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -13,10 +13,6 @@ class BbsController extends Controller
     }
 
     public function detail(Bb $bb) {
-        $s = $bb->title . "\r\n\r\n";
-        $s .= $bb->content . "\r\n";
-        $s .= $bb->price . " руб.\r\n";
-
-        return response($s)->header('Content-Type', 'text/plain');
+       return view('detail', ['bb' => $bb]);
     }
 }
---
+++ resources/views/detail.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+
+@section('title', $bb->title)
+
+@section('main')
+    <h2>{{ $bb->title }}</h2>
+    <p>{{ $bb->content }}</p>
+    <p>{{ $bb->price }} руб.</p>
+    <p><a href="/">На перечень объявлений</a></p>
+@endsection('main')
