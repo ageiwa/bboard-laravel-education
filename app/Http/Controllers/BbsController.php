5dd75a1901ac22d035b637d4eec02da761eb3031
Vlad Ageysha <ander123fakk@gmail.com>
2022-09-23 13:23:47 +0300

    Add index view for show data from db

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -8,16 +8,8 @@ use App\Models\Bb;
 class BbsController extends Controller
 {
     public function index() {
-        $bbs = Bb::latest()->get();
-        $s = "Объявления\r\n\r\n";
-
-        foreach ($bbs as $bb) {
-            $s .= $bb->title . "\r\n";
-            $s .= $bb->price . " руб.\r\n";
-            $s .= "\r\n";
-        }
-
-        return response($s)->header('Content-Type', 'text/plain');
+        $context = ['bbs' => Bb::latest()->get()];
+        return view('index', $context);
     }
 
     public function detail(Bb $bb) {
