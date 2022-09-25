e49e065f76c5d03ec4a7b44b76e6eb206331e323
Vlad Ageysha <ander123fakk@gmail.com>
2022-09-25 12:25:42 +0300

    Add route in link to add page

--- resources/views/bb_add.blade.php
+++ resources/views/bb_add.blade.php
@@ -10,11 +10,11 @@
         <input name="title" id="txtTitle" class="form-control">
     </div>
     <div class="form-group">
-        <label for="txtContent">Товар</label>
+        <label for="txtContent">Описание</label>
         <input name="content" id="txtContent" class="form-control">
     </div>
     <div class="form-group">
-        <label for="txtPrice">Товар</label>
+        <label for="txtPrice">Цена</label>
         <input name="price" id="txtPrice" class="form-control">
     </div>
     <input type="submit" class="btn btn-primary" value="Добавить">
